0128b300060face98b2f17b0cd167744089fefe1
Iain Dixon <iain@dixon.net>
2016-02-08 16:17:42 -0500

    Arcade Drive - Try One
    Sets robot to be driven by arcade drive.

--- Robot/src/org/usfirst/frc/team4829/robot/Robot.java
+++ Robot/src/org/usfirst/frc/team4829/robot/Robot.java
@@ -84,7 +84,7 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-        
+        driveTrain.arcadeDrive(moveStick);
     }
     
     /**
